1e2320597d4acb1feae7b8d1530b93a061d84019
Anna.Tokmina@urfu.me <Anna.Tokmina@urfu.me>
2024-10-06 13:16:24 +0500

    Задание 02, вариант 16 выполнен

---
+++ task03/task03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(("Введите число n");
+            var n = int.Parse(Console.ReadLine());
+
+            var chasi = n / 60;
+            var minuti = n / 3600;
+            var secundi = 
+
+        }
+    }
+}
