e2956f17793dfd957f3ae1a9a34ee74dab88bcb7
Anna.Tokmina@urfu.me <Anna.Tokmina@urfu.me>
2024-10-21 16:48:20 +0500

    Задание 7.1 выполнено, вариант 16

---
+++ task7/task7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var k = GetNumber("k");
+            var m = GetNumber("m");
+            var n = GetNumber("n");
+
+            if (IsStatTrue(k, m, n))
+                Console.WriteLine("Утверждение истинно");
+            else
+                Console.WriteLine("Утверждение ложно");
+
+            Console.ReadKey();
+        }
+
+        static bool IsStatTrue(int k, int m, int n)
+        {
+            return (k%2==0 && m%2==0 && n%2==0) || (k % 2 != 0 && m % 2 != 0 && n % 2 != 0);
+        }
+        static int GetNumber(string numberName) 
+        {
+            Console.WriteLine($"Введите число {numberName}");
+            return int.Parse(Console.ReadLine());
+
+        }
+    }
+}
