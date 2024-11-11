903ecd7dd38806855490a39bc9af6eac221f7009
Anna.Tokmina@urfu.me <Anna.Tokmina@urfu.me>
2024-11-11 16:33:57 +0500

    Задание 8, вариант 16 выполнен

---
+++ task8/task8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите значение аргумента функции");
+            var x = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"f({x}) = {MyFunction(x)}");
+
+            Console.ReadKey();
+        }
+
+        static Double MyFunction(double x)
+        {
+            double result;
+
+            if (x < -4) result=46;
+            else if ((x >= -4) && (x <= 2)) result=(3 * x * x - 2);
+            else result=10;
+
+            return result;
+        }
+    }
+}
