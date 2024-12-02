02a6723ae858e2799157af6c16bd85a32fafe8e1
Anna.Tokmina@urfu.me <Anna.Tokmina@urfu.me>
2024-12-02 16:36:59 +0500

    10.1 вариант 16

---
+++ task10/task10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            if (!TryInputtingNumber("Введите число n - количество фунтов:", out n) || n < 1)
+            {
+                Console.WriteLine("Пожалуйста, введите положительное целое число.");
+                Console.ReadKey();
+                return;
+            }
+
+            for (int pounds = 1; pounds <= n; pounds++)
+            {
+                double kilograms = pounds * 0.453;
+                Console.WriteLine($"{pounds} ф. = {kilograms} кг.");
+            }
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputtingNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+            return true;
+        }
+    }
+}
