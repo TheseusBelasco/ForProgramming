c0fca851308e6aa6917e80515dce328018209468
Anna.Tokmina@urfu.me <Anna.Tokmina@urfu.me>
2024-12-02 17:01:18 +0500

    10.2, вариант 16

---
+++ task10.2/task10.2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            if (!TryInputtingNumber("Введите количество действительных чисел:", out n))
+            {
+                Console.ReadKey();
+                return;
+            }
+
+            if (n < 1)
+            {
+                Console.WriteLine("Количество не может быть меньше единицы.");
+                Console.ReadKey();
+                return;
+            }
+
+            double sum = 0;
+
+            Console.WriteLine("Введите действительные числа через enter:");
+
+            for (var i = 0; i < n; i++)
+            {
+                
+                string input = Console.ReadLine();
+                if (double.TryParse(input, out double a))
+                {
+                    sum += a;
+                }
+                else Console.WriteLine("Ошибка ввода. Введите действительное число.");
+            }
+
+            double average = sum / n;
+            Console.WriteLine($"Среднее арифметическое: {average}");
+
+            Console.ReadKey();
+        }
+
+        static bool TryInputtingNumber(string message, out int number)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Ошибка ввода");
+                return false;
+            }
+            return true;
+        }
+    }
+}
